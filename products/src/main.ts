ed795b843fcbb8eab72115681999c049f588b1fc
NahumFGz <ngutierrez2507@gmail.com>
2025-04-21 15:35:12 -0500

    feat: :sparkles: Agreagando DTO y configuración global de filtrado de DTOs

--- products/src/main.ts
+++ products/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Solo traer lo q está definido en el DTO
+      forbidNonWhitelisted: true, //Error si hay mas datos q no están en el DTO
+    }),
+  )
+
   await app.listen(process.env.PORT ?? 3000)
 }
 
--- products/src/products/dto/create-product.dto.ts
+++ products/src/products/dto/create-product.dto.ts
@@ -1,1 +1,14 @@
-export class CreateProductDto {}
+import { Type } from 'class-transformer'
+import { IsNumber, IsString } from 'class-validator'
+
+export class CreateProductDto {
+  @IsString()
+  name: string
+
+  @IsString()
+  description: string
+
+  @IsNumber()
+  @Type(() => Number)
+  price: number
+}
