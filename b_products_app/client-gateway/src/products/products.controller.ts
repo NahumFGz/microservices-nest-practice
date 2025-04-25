93d98c623d5508ef20268611753afcec129b5894
NahumFGz <ngutierrez2507@gmail.com>
2025-04-25 03:58:02 -0500

    feat: :sparkles: Agregando los controlls

--- b_products_app/client-gateway/src/products/products.controller.ts
+++ b_products_app/client-gateway/src/products/products.controller.ts
@@ -1,6 +1,39 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common'
 
 @Controller('products')
 export class ProductsController {
   constructor() {}
+
+  @Post()
+  createProduct() {
+    return 'Crea un producto'
+  }
+
+  @Get()
+  findAllProducts() {
+    return 'Esta función regresa varios productos'
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return 'Esta función regresa el producto ' + id
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return 'Esta función elimina el producto ' + id
+  }
+
+  @Patch(':id')
+  patchProduct(@Param('id') _id: string, @Body() _body: any) {
+    return 'Esta función actualiza el producto'
+  }
 }
