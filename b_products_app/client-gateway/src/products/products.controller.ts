943a93e2fc654f9d2c7fb7a58a33d754956b0528
NahumFGz <ngutierrez2507@gmail.com>
2025-04-28 00:00:49 -0500

    feat: :sparkles: agregando el post y patch

--- b_products_app/client-gateway/src/products/products.controller.ts
+++ b_products_app/client-gateway/src/products/products.controller.ts
@@ -14,6 +14,8 @@ import { ClientProxy, RpcException } from '@nestjs/microservices'
 import { firstValueFrom } from 'rxjs'
 import { PaginationDto } from 'src/common'
 import { PRODUCT_SERVICE } from 'src/config'
+import { CreateProductDto } from './dto/create-product.dto'
+import { UpdateProductDto } from './dto/update-product.dto'
 
 @Controller('products')
 export class ProductsController {
@@ -22,8 +24,8 @@ export class ProductsController {
   ) {}
 
   @Post()
-  createProduct() {
-    return 'Crea un producto'
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send({ cmd: 'create_product' }, createProductDto)
   }
 
   @Get()
@@ -53,7 +55,10 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  patchProduct(@Param('id') _id: string, @Body() _body: any) {
-    return 'Esta función actualiza el producto'
+  patchProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsClient.send({ cmd: 'update_product' }, updateProductDto)
   }
 }
