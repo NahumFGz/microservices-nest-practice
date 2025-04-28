196701199781a344233250bb48a410cc929f83a3
NahumFGz <ngutierrez2507@gmail.com>
2025-04-27 23:46:21 -0500

    feat: :sparkles: Mejorando el tipado de custom exceptions

--- b_products_app/client-gateway/src/common/exceptions/rpc-custom-exception.filter.ts
+++ b_products_app/client-gateway/src/common/exceptions/rpc-custom-exception.filter.ts
@@ -1,21 +1,31 @@
 import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common'
 import { RpcException } from '@nestjs/microservices'
+import { Response } from 'express'
+
+interface RpcError {
+  status: number | string
+  message: string
+}
 
 @Catch(RpcException)
 export class RpcCustomExceptionFilter implements ExceptionFilter {
   catch(exception: RpcException, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
-    const response = ctx.getResponse()
+    const response = ctx.getResponse<Response>()
 
     const rpcError = exception.getError()
 
     if (
       typeof rpcError === 'object' &&
+      rpcError !== null &&
       'status' in rpcError &&
       'message' in rpcError
     ) {
-      const status = rpcError.status
-      return response.status(status).json(rpcError)
+      const { status, message } = rpcError as RpcError
+
+      const statusCode = isNaN(Number(status)) ? 400 : Number(status)
+
+      return response.status(statusCode).json({ status: statusCode, message })
     }
 
     response.status(400).json({
