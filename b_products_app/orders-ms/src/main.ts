03855a47785483f87af4722e700e4020f3422557
NahumFGz <ngutierrez2507@gmail.com>
2025-04-29 19:17:29 -0500

    feat: :sparkles: Agregando configuración de microservicio a orders

--- b_products_app/orders-ms/src/main.ts
+++ b_products_app/orders-ms/src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { envs } from './config/envs'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const logger = new Logger('Orders-Main')
 
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  )
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,8 +24,8 @@ async function bootstrap() {
     }),
   )
 
-  await app.listen(envs.port)
-  logger.log(`Products Microservice running on port ${envs.port}`)
+  await app.listen()
+  logger.log(`Orders Microservice running on port ${envs.port}`)
 }
 
 void bootstrap()
