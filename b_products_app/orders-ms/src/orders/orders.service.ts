b6d29853fdb6cf6a9d935359c494ce536810a422
NahumFGz <ngutierrez2507@gmail.com>
2025-04-30 06:51:13 -0500

    feat: :sparkles: Agregando el detalle a los services

--- b_products_app/orders-ms/src/orders/orders.service.ts
+++ b_products_app/orders-ms/src/orders/orders.service.ts
@@ -123,7 +123,16 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
   async findOne(id: string) {
     const order = await this.order.findFirst({
-      where: { id: id },
+      where: { id },
+      include: {
+        OrderItem: {
+          select: {
+            price: true,
+            quantity: true,
+            productId: true,
+          },
+        },
+      },
     })
 
     if (!order) {
@@ -133,7 +142,19 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
       })
     }
 
-    return order
+    const productIds = order.OrderItem.map((orderItem) => orderItem.productId)
+    const products: any[] = await firstValueFrom(
+      this.productsClient.send({ cmd: 'validate_products' }, productIds),
+    )
+
+    return {
+      ...order,
+      OrderItem: order.OrderItem.map((orderItem) => ({
+        ...orderItem,
+        name: products.find((product) => product.id === orderItem.productId)
+          .name,
+      })),
+    }
   }
 
   async changeStatus(changeOrderStatusDto: ChangeOrderStatusDto) {
